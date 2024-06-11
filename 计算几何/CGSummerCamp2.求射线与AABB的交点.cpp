7dc6d4a1802aca22261df17e1bfd570b89ec00d7
YXHXianYu <2943003@qq.com>
2024-06-11 17:36:52 +0800

    aabb complete

--- 计算几何/CGSummerCamp2.求射线与AABB的交点.cpp
+++ 计算几何/CGSummerCamp2.求射线与AABB的交点.cpp
@@ -7,17 +7,21 @@ using namespace std;
 /* 基础板子 */
 
 typedef double f64; // 换longdouble时，需要同时替换alpha函数和getTheta函数
-const f64 EPS = 1e-9;
+const f64      EPS = 1e-9;
 
-inline int sign(f64 a) { return a < -EPS ? -1 : a > EPS; }
+inline int sign(f64 a) {
+    return a < -EPS ? -1 : a > EPS;
+}
 
-inline int cmp(f64 a, f64 b) { return sign(a - b); }
+inline int cmp(f64 a, f64 b) {
+    return sign(a - b);
+}
 
 // 点P
 struct P {
     f64 x, y;
-    P(): x(0), y(0) {}
-    P(f64 _x, f64 _y): x(_x), y(_y) {}
+    P() : x(0), y(0) {}
+    P(f64 _x, f64 _y) : x(_x), y(_y) {}
     P operator+(P p) const { return {x + p.x, y + p.y}; }
     P operator-(P p) const { return {x - p.x, y - p.y}; }
     P operator*(f64 d) const { return {x * d, y * d}; }
@@ -25,31 +29,29 @@ struct P {
 
     bool operator<(P p) const {
         int c = cmp(x, p.x);
-        if(c) return c == -1;
+        if (c) return c == -1;
         return cmp(y, p.y) == -1;
     }
 
     // 这里的比较可能不满足传递性，因为这是基于EPS的比较
-    bool operator==(P p) const {
-        return cmp(x, p.x) == 0 && cmp(y, p.y) == 0;
-    }
+    bool operator==(P p) const { return cmp(x, p.x) == 0 && cmp(y, p.y) == 0; }
 
     f64 dot(P p) { return x * p.x + y * p.y; }
     f64 det(P p) { return x * p.y - y * p.x; }
 
-    f64 distTo(P p) const { return (*this-p).abs(); }
+    f64 distTo(P p) const { return (*this - p).abs(); }
     // return value in [-pi, pi]; longdouble ver is atan2l(y, x)
-    f64 alpha() { return atan2(y, x); }
+    f64  alpha() { return atan2(y, x); }
     void read() { cin >> x >> y; }
-    f64 abs() const { return sqrt(abs2()); }
-    f64 abs2() const { return x * x + y * y; }
-    P unit() { return *this / abs(); }
-    P rot90() { return P(-y, x); }
-    P rot(f64 an) { return {x*cos(an) - y*sin(an), x*sin(an) + y*cos(an)}; }
-    int quad() const { return sign(y) == 1 || (sign(y) == 0 && sign(x) >= 0); }
+    f64  abs() const { return sqrt(abs2()); }
+    f64  abs2() const { return x * x + y * y; }
+    P    unit() { return *this / abs(); }
+    P    rot90() { return P(-y, x); }
+    P    rot(f64 an) { return {x * cos(an) - y * sin(an), x * sin(an) + y * cos(an)}; }
+    int  quad() const { return sign(y) == 1 || (sign(y) == 0 && sign(x) >= 0); }
 };
 
-#define cross(p1, p2, p3) ((p2.x-p1.x)*(p3.y-p1.y)-(p3.x-p1.x)*(p2.y-p1.y))
+#define cross(p1, p2, p3) ((p2.x - p1.x) * (p3.y - p1.y) - (p3.x - p1.x) * (p2.y - p1.y))
 #define crossOp(p1, p2, p3) (sign(cross(p1, p2, p3)))
 
 // 判断 直线p1p2 和 直线q1q2 是否相交
@@ -66,7 +68,7 @@ P getLL(P p1, P p2, P q1, P q2) {
 
 // 点m 是否在 a 和 b 之间
 bool isMiddle(f64 m, f64 a, f64 b) {
-    if(a > b) swap(a, b);
+    if (a > b) swap(a, b);
     return cmp(a, m) <= 0 && cmp(m, b) <= 0;
 }
 
@@ -83,13 +85,13 @@ bool onSeg(P p1, P p2, P q) {
 // Main
 void work() {
 
-    freopen("input.txt", "r", stdin);
+    // freopen("input.txt", "r", stdin);
 
     size_t n, m;
     std::cin >> n >> m;
 
     using AABB = std::array<P, 2>;
-    using Ray = std::array<P, 2>;
+    using Ray  = std::array<P, 2>;
 
     std::vector<AABB> aabb(n);
     for (size_t i = 0; i < n; i++) {
@@ -103,66 +105,54 @@ void work() {
         ray[i][1].read();
     }
 
-    auto getIntersectionOfEdgeAndRay
-        = [&](const P& p1, const P& p2, const Ray& ray) -> std::pair<bool, P> {
-        
+    auto preGetIntersectionOfEdgeAndRay = [&](const P& p1, const P& p2, const Ray& ray) -> bool {
         P o = ray[0];
         P x = ray[0] + ray[1];
 
         // o in edge
         if (onSeg(p1, p2, o)) {
             // assert(false);
+            // return std::make_pair(true, o);
         }
 
         // same line (4)
-        if (crossOp(o, x, p1) == 0 && crossOp(o, x, p2) == 0) {
-            return std::make_pair(false, P());
-        }
+        if (crossOp(o, x, p1) == 0 && crossOp(o, x, p2) == 0) { return false; }
+        return true;
+    };
+
+    auto getIntersectionOfEdgeAndRay = [&](const P& p1, const P& p2, const Ray& ray) -> std::pair<bool, P> {
+        P o = ray[0];
+        P x = ray[0] + ray[1];
 
         // parellel (1)
-        if (cmp(cross(o, x, p1), cross(o, x, p2)) == 0 || !isLL(o, x, p1, p2)) {
-            return std::make_pair(false, P());
-        }
+        if (!isLL(o, x, p1, p2)) { return std::make_pair(false, P()); }
 
         // get intersection
         P inter = getLL(o, x, p1, p2);
 
         // inter isn't in segment (1)
-        if (!isMiddle(inter, p1, p2)) {
-            return std::make_pair(false, P());
-        }
-        if (sign(ray[1].x) != cmp(inter.x, ray[0].x)) {
-            return std::make_pair(false, P());
-        }
-        if (sign(ray[1].y) != cmp(inter.y, ray[0].y)) {
-            return std::make_pair(false, P());
-        }
+        if (!isMiddle(inter, p1, p2)) { return std::make_pair(false, P()); }
+        if (sign(ray[1].x) != cmp(inter.x, ray[0].x)) { return std::make_pair(false, P()); }
+        if (sign(ray[1].y) != cmp(inter.y, ray[0].y)) { return std::make_pair(false, P()); }
 
         // (2)
         return std::make_pair(true, inter);
     };
 
-    auto getIntersectionOfAABBAndRay
-        = [&](const AABB& aabb, const Ray& ray) -> std::vector<P> {
-        // 1. rebuild 4 edge
-        P p[4] = {
-            aabb[0],
-            P(aabb[0].x, aabb[1].y),
-            aabb[1],
-            P(aabb[1].x, aabb[0].y)
-        };
+    auto getIntersectionOfAABBAndRay = [&](const AABB& aabb, const Ray& ray) -> std::vector<P> {
+        P p[4] = {aabb[0], P(aabb[0].x, aabb[1].y), aabb[1], P(aabb[1].x, aabb[0].y)};
 
-        // 2. iterate 4 edge
         std::vector<P> ans;
         for (size_t i = 0; i < 4; i++) {
-            auto res = getIntersectionOfEdgeAndRay(p[i], p[(i+1)%4], ray);
-            if (res.first) {
-                ans.push_back(res.second);
-                // std::cout << " - " << "(" << p[i].x << ", " << p[i].y << ") - (";
-                // std::cout << p[(i+1)%4].x << ", " << p[(i+1)%4].y << "): ";
-                // std::cout << res.second.x << ", " << res.second.y << std::endl;
-            }
+            auto res = preGetIntersectionOfEdgeAndRay(p[i], p[(i + 1) % 4], ray);
+            if (!res) { return ans; } // (4)
         }
+        for (size_t i = 0; i < 4; i++) {
+            auto res = getIntersectionOfEdgeAndRay(p[i], p[(i + 1) % 4], ray);
+            if (res.first) { ans.push_back(res.second); }
+        }
+        stable_sort(ans.begin(), ans.end(), [&](const P& a, const P& b) { return cmp(ray[0].distTo(a), ray[0].distTo(b)) < 0; });
+        ans.erase(std::unique(ans.begin(), ans.end()), ans.end());
         return ans;
     };
 
@@ -170,20 +160,18 @@ void work() {
         std::vector<P> ans;
         for (size_t j = 0; j < n; j++) {
             auto res = getIntersectionOfAABBAndRay(aabb[j], ray[i]);
-            for (const auto& x: res) {
+            for (const auto& x : res) {
                 ans.push_back(x);
             }
         }
         if (ans.size() > 0) {
-            stable_sort(ans.begin(), ans.end(), [&](const P& a, const P& b) {
-                return cmp(ray[i][0].distTo(a), ray[i][0].distTo(b)) < 0;
-            });
-            ans.erase(std::unique(ans.begin(), ans.end()), ans.end());
+            stable_sort(ans.begin(), ans.end(), [&](const P& a, const P& b) { return cmp(ray[i][0].distTo(a), ray[i][0].distTo(b)) < 0; });
+            // ans.erase(std::unique(ans.begin(), ans.end()), ans.end());
 
             std::cout << ans.size() << " ";
             std::cout << std::fixed << std::setprecision(2);
-            for (const auto& x: ans) {
-                std::cout << (x.x+EPS) << " " << (x.y+EPS) << " ";
+            for (const auto& x : ans) {
+                std::cout << (x.x + EPS) << " " << (x.y + EPS) << " ";
             }
             std::cout << '\n';
         } else {
@@ -198,7 +186,7 @@ signed main() {
 
     int t = 1;
     // cin >> t;
-    while(t--) {
+    while (t--) {
         work();
     }
 
